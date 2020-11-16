04461b6577f776558ad177107cff12314bf66cf0
Katie <katieberrym@gmail.com>
2020-11-16 08:05:22 -0800

    added api keys to .env and added starter data to favorites.js

---
+++ data/favorites.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Portland',
+    city_api_id: 32,
+    years: "2000:2005",
+    aggregation: "monthly"
+  },
+  {
+    name: 'Portland',
+    city_api_id: 32,
+    years: "1950:2005",
+    aggregation: "monthly"
+  },
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const favorites = require('./favorites.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -18,30 +18,30 @@ async function run() {
                       VALUES ($1, $2)
                       RETURNING *;
                   `,
-        [user.email, user.hash]);
+          [user.email, user.hash]);
       })
     );
-      
+
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorites.map(fave => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO fave (name, city_api_id, years, aggregation, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+          [fave.name, fave.city_api_id, fave.years, fave.aggregation, user.id]);
       })
     );
-    
+
 
     console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
   }
-  catch(err) {
+  catch (err) {
     console.log(err);
   }
   finally {
     client.end();
   }
-    
+
 }
